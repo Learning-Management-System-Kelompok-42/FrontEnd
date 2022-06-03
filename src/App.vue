e4f64469116618c4cf3ead15dbf8f71a14ee81ba
rizkiagus <rizki.setiawanagus@gmail.com>
2022-06-03 18:39:30 +0700

    feat: landingPage

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="app">
+    <landing />
+  </v-app>
+</template>
+
+<script>
+import landing from "@/views/LandingView.vue";
+export default {
+  name: "App",
+  components: {
+    landing,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
