5d3afde54d48d67412e974dc4f9c2bcad1e24c5d
rizkiagus <rizki.setiawanagus@gmail.com>
2022-07-11 19:13:05 +0700

    fix: route

--- src/views/User/Course/UserCourse.vue
+++ src/views/User/Course/UserCourse.vue
@@ -83,57 +83,64 @@
                   color="secondary"
                   >{{ value }}%</b
                 >
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-avatar
-                                class="mt-3"
-                                rounded
-                                height="80"
-                                color="grey6"
-                                size="100%"
-                            >
-                                <img src="@/assets/avajjm.png">
-                            </v-list-item-avatar>
-                            <v-row class="ma-auto mt-2">
-                                <v-rating
-                                    v-model="rating"
-                                    background-color="orange lighten-3"
-                                    color="orange"
-                                    half-increments
-                                    dense
-                                    size="15"    
-                                ></v-rating>
-                                <span class="grey--text text-caption">{{ rating }}</span>
-                            </v-row>
-                            <v-list-item-title class="text-h8 mb-1 pt-3">
-                                <b>Junior Javascript Mastery</b>
-                            </v-list-item-title>
-                            <p class="text-h18 my-auto gray--text">Kalian akan mempelajari mengenai bahasa Javascript yang paling dasar.
-                            </p>
-                        </v-list-item-content>
-                    </v-list-item>
-                    <div class="px-4 d-flex my-auto">
-                            <v-progress-linear 
-                                v-model="value"
-                                class="my-auto"
-                                height="5"
-                                rounded 
-                                color="secondary"
-                            ></v-progress-linear>
-                        <b class="text-h15 my-auto pl-2 secondary--text" color="secondary">{{value}}%</b>
-                    </div>
-                    <v-card-actions>
-                    <v-btn
-                        class="text-capitalize"
-                        to="urcourse/detail"
-                        text
-                        color="primary"
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-avatar
+                      class="mt-3"
+                      rounded
+                      height="80"
+                      color="grey6"
+                      size="100%"
                     >
-                        Lihat Detail <v-icon class="pt-1">mdi-chevron-right</v-icon>
-                    </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
+                      <img src="@/assets/avajjm.png" />
+                    </v-list-item-avatar>
+                    <v-row class="ma-auto mt-2">
+                      <v-rating
+                        v-model="rating"
+                        background-color="orange lighten-3"
+                        color="orange"
+                        half-increments
+                        dense
+                        size="15"
+                      ></v-rating>
+                      <span class="grey--text text-caption">{{ rating }}</span>
+                    </v-row>
+                    <v-list-item-title class="text-h8 mb-1 pt-3">
+                      <b>Junior Javascript Mastery</b>
+                    </v-list-item-title>
+                    <p class="text-h18 my-auto gray--text">
+                      Kalian akan mempelajari mengenai bahasa Javascript yang
+                      paling dasar.
+                    </p>
+                  </v-list-item-content>
+                </v-list-item>
+                <div class="px-4 d-flex my-auto">
+                  <v-progress-linear
+                    v-model="value"
+                    class="my-auto"
+                    height="5"
+                    rounded
+                    color="secondary"
+                  ></v-progress-linear>
+                  <b
+                    class="text-h15 my-auto pl-2 secondary--text"
+                    color="secondary"
+                    >{{ value }}%</b
+                  >
+                </div>
+                <v-card-actions>
+                  <v-btn
+                    class="text-capitalize"
+                    to="urcourse/detail"
+                    text
+                    color="primary"
+                  >
+                    Lihat Detail <v-icon class="pt-1">mdi-chevron-right</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </div>
+            </v-card>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
